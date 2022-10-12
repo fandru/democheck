1285ca0344f614696ea06a012ec6eb275d99baf1
fnelsonanburaj <39556131+fandru@users.noreply.github.com>
2022-10-12 18:32:51 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,8 +4,8 @@
 
 
 
-resource "google_storage_bucket" "gcs_bucket_for_mercaury_demo" {
-    name = "mercaury_storage_demo"
+resource "google_storage_bucket" "gcs_bucket_for_mercaury_de" {
+    name = "mercaury_storage_de"
     storage_class = "REGIONAL"
     project = "sampledemoproject-365311"
     location = "us-central1"  
@@ -13,11 +13,4 @@ resource "google_storage_bucket" "gcs_bucket_for_mercaury_demo" {
 }
 
 
-resource "google_storage_bucket" "gcs_bucket_for_mercaury_demo1_drive" {
-    name = "mercaury_storage_demo_drive"
-    storage_class = "REGIONAL"
-    project = "sampledemoproject-365311"
-    location = "us-central1"  
 
-}
-  
