fa5ba43def9a5cbc330e3c36a0280c72fa514195
fnelsonanburaj <39556131+fandru@users.noreply.github.com>
2022-10-12 18:25:56 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,4 +11,13 @@ resource "google_storage_bucket" "gcs_bucket_for_mercaury_demo" {
     location = "us-central1"  
 
 }
+
+
+resource "google_storage_bucket" "gcs_bucket_for_mercaury_demo1_drive" {
+    name = "mercaury_storage_demo_drive"
+    storage_class = "REGIONAL"
+    project = "sampledemoproject-365311"
+    location = "us-central1"  
+
+}
   
